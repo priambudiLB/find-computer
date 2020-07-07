d21dac1171c689d3dc2f710c11168357e37a8314
Bagas <priambudibagaskara@gmail.com>
2020-07-07 11:24:24 +0700

    update site data

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -31,11 +31,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `find-computer`,
+        short_name: `Find Computer`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#41403e`,
+        theme_color: `#41403e`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
